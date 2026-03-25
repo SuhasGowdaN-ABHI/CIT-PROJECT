3c8f54baa089eb92abab656357a3fef84863c8cc
SuhasGowdaN-ABHI <suhasgowdan45@gmail.com>
2026-03-25 14:46:38 +0530

    initial commit for java project

---
+++ stringconcatenation.java
@@ -0,0 +1,8 @@
+public class stringconcatenation {
+    public static void main(String[] args) {
+        String name = "xxx";
+        int age = 25;
+        System.out.println("Name: " + name + "Age: " + age);
+    }
+
+}
---
+++ typecastingoperator.java
@@ -0,0 +1,11 @@
+public class typecastingoperator {
+    public static void main(String[] args) {
+        double d = 9.78;
+        // explicit type conversion
+        int num = (int) d;
+        System.out.println("Double values before conversion: " + d);
+        System.out.println("After explicit type of conversion: " + num);
+        // implicit type conversion
+    }
+
+}
