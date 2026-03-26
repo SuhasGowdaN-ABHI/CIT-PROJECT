0ca6acec79e6783b7a9ea1c89b133efcd72928c7
SuhasGowdaN-ABHI <suhasgowdan45@gmail.com>
2026-03-26 12:03:20 +0530

    updated control structures

---
+++ nestedif.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class nestedif {
+    public static void main(String[] args) {
+        // int age=65;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter age");
+        int age = sc.nextInt();
+
+        if(age>=18)
+        {
+
+            if(age>=65){
+                System.out.println("Eligible to avail government fund");
+            }
+            else
+            System.out.println("we are not eligible to vote and avail government scheme");
+        }
+        else
+        System.out.println("we are not eligible to voting");
+    }
+}
